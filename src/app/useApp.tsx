05d76036a6a6b75d07d2d1cf6306c833f93049af
h12t20 <sergdiamond@ymail.com>
2023-09-03 16:26:14 +0300

    login, logout, auth me

--- src/app/useApp.tsx
+++ src/app/useApp.tsx
@@ -2,6 +2,7 @@ import {TaskType} from "../api/task-api";
 import {useAppDispatch, useAppSelector} from "./hook";
 import {useEffect} from "react";
 import {initializeAppTC} from "../features/login/auth-reducer";
+
 export type TasksStateType = {
     [key: string]: Array<TaskType>
 }
