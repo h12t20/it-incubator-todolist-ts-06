cb4e0ecc45f30e6d60c8b1488d8defb4ea194783
h12t20 <sergdiamond@ymail.com>
2023-11-16 13:51:34 +0300

    api key

--- src/common/api/instance.ts
+++ src/common/api/instance.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
   withCredentials: true,
-  headers: { "API-KEY": "f866834e-8cfc-4d13-b77d-5b9822baa8e3" },
+  headers: { "API-KEY": "c4e2e779-7576-4ba3-8689-ca30206134b3" },
 });
