738d2455c19c6c798dcc96f0beaef7806c4f8dc2
h12t20 <sergdiamond@ymail.com>
2023-11-16 17:23:46 +0300

    change router to hash

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import s from "app/App.module.scss";
 import { TodolistList } from "features/TodolistList/TodolistList";
 import { Header } from "features/header/Header";
 import { ErrorSnackbar } from "common/components/errorsnachbar/ErrorSnackbar";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Login } from "features/login/Login";
 import { CircularProgress } from "@mui/material";
 import { useApp } from "./useApp";
@@ -31,14 +31,14 @@ export const App = () => {
         <Header theme={theme} handleOpen={handleOpenSwitch} />
       </div>
       <div className={s.Body}>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<TodolistList />} />
             <Route path="/login" element={<Login />} />
             <Route path="/404" element={<h1>404: PAGE NOT FOUND</h1>} />
             <Route path="*" element={<Navigate to="/404" />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </div>
   );
